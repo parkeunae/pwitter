85e923cc1227fe87331c0fe8d197206d964fe955
parkeunae <eapark108@gmail.com>
2021-02-10 23:46:56 +0900

    Add log out function

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Profile from 'routes/Profile';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
 import Navigation from './Navigation';
@@ -10,13 +11,21 @@ const AppRouter = ({ isLoggedIn }) => {
             {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
+                    <>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                    </>
                 ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                        <Redirect from="*" to="/" />
+                    </>
                 )}
             </Switch>
         </Router>
