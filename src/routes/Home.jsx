7f198d0b341a3eacdc50c3edca592e08388b45cf
parkeunae <eapark108@gmail.com>
2021-02-15 20:16:56 +0900

    Add upload file func

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { dbService } from 'fbase';
+import {v4 as uuidv4} from 'uuid';
+import { dbService, storageService } from 'fbase';
 import Tweet from 'components/Tweet';
 
 const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState('');
     const [tweets, setTweets] = useState([]);
+    const [attachment, setAttachment] = useState('');
 
     useEffect(() => {
         dbService.collection('tweets').onSnapshot((snapshot) => {
@@ -23,18 +25,44 @@ const Home = ({ userObj }) => {
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        await dbService.collection("tweets").add({
+        let attachmentUrl = '';
+
+        if (attachment !== '') {
+            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment, 'data_url');
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
+
+        const tweetObj = {
             text: tweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
-        });
+            attachmentUrl,
+        };
+        await dbService.collection("tweets").add(tweetObj);
+
         setTweet('');
+        setAttachment('');
     }
 
     const onChange = (event) => {
         const { target: { value } } = event;
         setTweet(value);
     }
+
+    const onFileChange = (event) => {
+        const { target: { files } } = event;
+
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const { currentTarget: { result } } = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+
+    const onClearAttachment = () => setAttachment(null);
 
     return (
         <div>
@@ -46,7 +74,14 @@ const Home = ({ userObj }) => {
                     value={tweet}
                     onChange={onChange}
                 />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Tweet"/>
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" alt="preview" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {tweets.map((doc) => (
