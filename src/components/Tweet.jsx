f8f309822cd9dd0cf72292572a67200cafe1b9b1
parkeunae <eapark108@gmail.com>
2021-02-16 23:31:36 +0900

    Add image url delete

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { dbService } from "fbase";
+import React, { useState } from 'react';
+import { dbService, storageService } from 'fbase';
 
 const Tweet = ({ tweetObj, isOwner }) => {
     const [isEditing, setIsEditing] = useState(false);
@@ -9,8 +9,9 @@ const Tweet = ({ tweetObj, isOwner }) => {
         const ok = window.confirm('Are you sure want to delete this tweet?');
         if (ok) {
             await dbService.doc(`tweets/${tweetObj.id}`).delete();
+            await storageService.refFromURL(tweetObj.attachmentUrl).delete();
         }
-    }
+    };
 
     const toggleEditing = () => setIsEditing((prev) => !prev);
 
@@ -22,45 +23,34 @@ const Tweet = ({ tweetObj, isOwner }) => {
         });
 
         setIsEditing(false);
-    }
+    };
 
     const onChange = (event) => {
-        const { target: { value } } = event;
+        const {
+            target: { value },
+        } = event;
         setNewTweet(value);
-    }
+    };
 
     return (
-        <div  key={tweetObj.id}>
+        <div key={tweetObj.id}>
             {isEditing ? (
                 <>
                     <form onSubmit={onSubmit}>
-                        <input
-                            type="text"
-                            placeholder="Edit your tweet"
-                            value={newTweet}
-                            onChange={onChange}
-                            required
-                        />
-                        <input type="submit" value="Update Tweet"/>
+                        <input type="text" placeholder="Edit your tweet" value={newTweet} onChange={onChange} required />
+                        <input type="submit" value="Update Tweet" />
                     </form>
                     <button onClick={toggleEditing}>Cancel</button>
                 </>
             ) : (
                 <>
                     <h4>{tweetObj.text}</h4>
-                    {tweetObj.attachmentUrl && (
-                        <img
-                            src={tweetObj.attachmentUrl}
-                            width="50px"
-                            height="50px"
-                            alt=""
-                        />
-                    )}
+                    {tweetObj.attachmentUrl && <img src={tweetObj.attachmentUrl} width="50px" height="50px" alt="" />}
                     {isOwner && (
-                    <>
-                        <button onClick={onDeleteClick}>Delete Tweet</button>
-                        <button onClick={toggleEditing}>Edit Tweet</button>
-                    </>
+                        <>
+                            <button onClick={onDeleteClick}>Delete Tweet</button>
+                            <button onClick={toggleEditing}>Edit Tweet</button>
+                        </>
                     )}
                 </>
             )}
