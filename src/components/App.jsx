30901278f6c30d7148382a6fdb74aac50162b8c4
parkeunae <eapark108@gmail.com>
2021-02-18 20:44:40 +0900

    Fix logout bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,18 @@ import { authService } from 'fbase';
 
 function App() {
     const [init, setInit] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userObj, setUserObj] = useState(null);
 
     useEffect(() => {
         authService.onAuthStateChanged((user) => {
             if (user) {
-                setIsLoggedIn(true);
                 setUserObj({
                     displayName: user.displayName,
                     uid: user.uid,
                     updateProfile: (args) => user.updateProfile(args),
                 });
             } else {
-                setIsLoggedIn(false);
+                setUserObj(null);
             }
             setInit(true);
         });
@@ -35,7 +33,7 @@ function App() {
     return (
         <>
             {init ? (
-                <AppRouter refreshUser={refreshUser} isLoggedIn={isLoggedIn} userObj={userObj} />
+                <AppRouter refreshUser={refreshUser} isLoggedIn={Boolean(userObj)} userObj={userObj} />
             ) : (
                 'Initializing...'
             )}
